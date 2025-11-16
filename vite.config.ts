dd76e338decd5c588bc91d6ed416347a683aac64
Kyle Mathewson <kylemath@gmail.com>
2025-11-16 16:40:36 -0700

    Configure Vite for production and build output

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/InstaFaceReadX/' : '/',
+  build: {
+    outDir: 'docs',
+    assetsDir: 'assets',
+  },
   server: {
     port: 3000,
     open: true
